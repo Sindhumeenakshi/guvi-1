69d6209ab25d645deb772b6ade9aa59e32cf3c17
Iswaryadevi98 <51690701+Iswaryadevi98@users.noreply.github.com>
2019-06-12 11:20:04 +0530

    Update vowel.py

--- codeketa/vowel.py
+++ codeketa/vowel.py
@@ -1,6 +1,9 @@
 x=input()
 l1=['a','e','i','o','u']
+l2=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 if x in l1:
  print("Vowel")
-else:
+elif x in l2:
  print("Consonant")
+else:
+  print("invalid")
